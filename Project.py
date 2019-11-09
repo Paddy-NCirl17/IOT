3ec7bce7d55b6333e8f7cd85154c0a84d23cc932
Patrick Moran <p.jmoran@icloud.com>
2019-11-09 11:04:21 +0000

    Added Buzzer rev3

--- Project.py
+++ Project.py
@@ -30,9 +30,9 @@ dht_sensor_type = 0
 buzzer = 8
 
 while True:
-        # get the temperature and Humidity from the DHT sensor
-		[ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
-		print("temp =", temp,)
-        if temp > 21:
-            grovepi.digitalWrite(buzzer,1)
-            time.sleep(1)
+    # get the temperature and Humidity from the DHT sensor
+	[ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
+	print("temp =", temp,)
+    if temp > 21:
+        grovepi.digitalWrite(buzzer,1)
+        time.sleep(1)
