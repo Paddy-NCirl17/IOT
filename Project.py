2cedb8e85887137b81e5b547282ac761edcd1a81
Patrick Moran <p.jmoran@icloud.com>
2019-11-09 10:48:42 +0000

    Temperature update3

--- Project.py
+++ Project.py
@@ -1,73 +1,33 @@
-#!/usr/bin/env python
-#
-# GrovePi Library for using the Grove - Temperature&Humidity Sensor (http://www.seeedstudio.com/depot/Grove-TemperatureHumidity-Sensor-HighAccuracy-Mini-p-1921.html)
-#
-# The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:  http://www.dexterindustries.com/GrovePi
-#
-# Have a question about this library?  Ask on the forums here:  http://forum.dexterindustries.com/c/grovepi
-#
-# Released under the MIT license (http://choosealicense.com/licenses/mit/).
-# For more information see https://github.com/DexterInd/GrovePi/blob/master/LICENSE
-#################################################################################################################################################
-# NOTE:
-# The software for this sensor is still in development and might make your GrovePi unuable as long as this sensor is connected with the GrovePi
-#################################################################################################################################################
-import time,sys
-import RPi.GPIO as GPIO
-import smbus
+'''
+The MIT License (MIT)
 
-debug = 0
-# use the bus that matches your raspi version
-rev = GPIO.RPI_REVISION
-if rev == 2 or rev == 3:
-    bus = smbus.SMBus(1)
-else:
-    bus = smbus.SMBus(0)
+GrovePi for the Raspberry Pi: an open source platform for connecting Grove Sensors to the Raspberry Pi.
+Copyright (C) 2017  Dexter Industries
 
-class th02:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-	ADDRESS = 0x40
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-	TH02_REG_STATUS = 0x00
-	TH02_REG_DATA_H = 0x01
-	TH02_REG_DATA_L = 0x02
-	TH02_REG_CONFIG = 0x03
-	TH02_REG_ID = 0x11
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+from grovepi import *
 
-	TH02_STATUS_RDY_MASK = 0x01
+dht_sensor_port = 7
 
-	TH02_CMD_MEASURE_HUMI = [0x01]
-	TH02_CMD_MEASURE_TEMP = [0x11]
-
-	SUCCESS = 0
-
-	def getTemperature(self):
-		bus.write_i2c_block_data(self.ADDRESS, self.TH02_REG_CONFIG, self.TH02_CMD_MEASURE_TEMP)
-
-		while 1:
-			status=self.getStatus()
-			if debug:
-				print("st:",status)
-			if status:
-				break
-		t_raw=bus.read_i2c_block_data(self.ADDRESS, self.TH02_REG_DATA_H,3)
-		if debug:
-			print(t_raw)
-		temperature = (t_raw[1]<<8|t_raw[2])>>2
-		return (temperature/32.0)-50.0
-
-
-	def getStatus(self):
-		status=bus.read_i2c_block_data(self.ADDRESS, self.TH02_REG_STATUS,1)
-		if debug:
-			print(status)
-		if status[0] & self.TH02_STATUS_RDY_MASK != 1:
-			return 1
-		else:
-			return 0
-
-if __name__ == "__main__":
-	t= th02()
-	while True:
-		print(t.getTemperature())
-		time.sleep(.5)
+while True:
+    try:
+        # get the temperature and Humidity from the DHT sensor
+		[ temp ] = dht(dht_sensor_port,dht_sensor_type)
+		print("temp =", temp,)
