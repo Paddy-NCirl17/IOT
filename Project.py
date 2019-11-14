b8f9556d11c444920f18730267069097a6961f1c
Patrick Moran <p.jmoran@icloud.com>
2019-11-14 18:52:14 +0000

    update20

--- Project.py
+++ Project.py
@@ -95,7 +95,8 @@ while True:
             
         else:
          alarm = "Alarm is not active"
-         print(alarm)
+            print("alarm",alarm)
+            print("temp",temp)
 
         if button_sensor ==1:
          reset = 1
@@ -107,6 +108,7 @@ while True:
         fireAlarm["Temperature"] = temp
         fireAlarm["Time"] = timeStamp
         fireAlarm["Noise"] = roomNoise
+        fireAlarm["Noise_Value"] = sensor_value
         fireAlarm["Reset"] = reset
         
 
